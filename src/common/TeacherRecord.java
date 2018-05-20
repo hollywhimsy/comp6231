ea266018db19098373063a56ac1c3aa2b53126cd
Valeriu Caramanuta <kavaleriu@gmail.com>
2018-05-19 20:02:24 -0400

    getter and setters for Teacher record

--- src/common/TeacherRecord.java
+++ src/common/TeacherRecord.java
@@ -5,6 +5,46 @@ package common;
  */
 public class TeacherRecord extends Record {
 
+	private String address ;
+	
+	private String phone ;
+	
+	private String specialization;
+	
+	private String location;
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
 	public TeacherRecord(String firstName, String lastName) {
 		super(firstName, lastName);
 	}
