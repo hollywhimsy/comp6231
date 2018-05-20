2ca8e423dffe7e67e1b6c719aea662627a2b3442
hollywhimsy <hollyfryan@hotmail.com>
2018-05-19 20:24:47 -0400

    We completed this phase of the project with the server implementation and the instances of the classes teacher and student

---
+++ src/AddClass.java
@@ -0,0 +1,16 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class AddClass extends UnicastRemoteObject implements AddInterface {
+
+	public AddClass() throws Exception{
+		super();
+	}
+
+	public int add(int x, int y) throws RemoteException {
+	
+		return x + y;
+		
+	}
+}
+
