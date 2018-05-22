7dbdd7fd6f5bfbd8c1edd4b9305dc2fc46600f94
PancakeOy <39447361+PancakeOy@users.noreply.github.com>
2018-05-22 15:16:43 -0400

    Update AddInterface.java

--- src/AddInterface.java
+++ src/AddInterface.java
@@ -1,9 +1,19 @@
 package src;
 import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface AddInterface extends Remote{
 
 	public int add(int x, int y) throws RemoteException; 
+	
+	public void createTRecord(String firstName, String lastName, String address, String phone, String specialization, String location) throws RemoteException;
+	
+	public void createSRecord(String firstName, String lastName, String courseRegistered, String status, String statusDate) throws RemoteException;
+	
+	public void getRecordCounts() throws RemoteException;
+	
+	public void editRecord(String recordID, String fieldName, String newValue) throws RemoteException;
 	
 }
 
