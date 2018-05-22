e17fab5477687107d6b13c376f76557ee79215a0
PancakeOy <39447361+PancakeOy@users.noreply.github.com>
2018-05-22 16:36:20 -0400

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -42,12 +42,12 @@ public class Client extends Thread {
 	}
  public static void main(String[] args) throws Exception{
 	 
-	 Registry registry = LocateRegistry.getRegistry(2964); 
+// 	 Registry registry = LocateRegistry.getRegistry(2964); 
 	 
-	 AddInterface obj = (AddInterface) registry.lookup("Addition");
+// 	 AddInterface obj = (AddInterface) registry.lookup("Addition");
 	 
-	 int n = obj.add(5, 4); 
-	 System.out.println("Addition is : " + n);
+// 	 int n = obj.add(5, 4); 
+// 	 System.out.println("Addition is : " + n);
 	 
 	 run();
  }
