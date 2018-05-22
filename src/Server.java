f82785d03a30ce3832cd72cb9bc9be3ff6a5386f
PancakeOy <39447361+PancakeOy@users.noreply.github.com>
2018-05-22 16:35:52 -0400

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -37,9 +37,11 @@ public class Server extends Thread{
 
 	public static void main(String[] args) throws Exception{
 		
-		AddClass obj = new AddClass(); 
-		Registry registry = LocateRegistry.createRegistry(2964); 
-		registry.bind("Addition", obj);
-		System.out.println("Server is started");
+// 		AddClass obj = new AddClass(); 
+// 		Registry registry = LocateRegistry.createRegistry(2964); 
+// 		registry.bind("Addition", obj);
+// 		System.out.println("Server is started");
+		run();
+		
 	}
 }
