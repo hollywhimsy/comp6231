70600fa4dade654372b17b2cea09471e085be7e6
PancakeOy <39447361+PancakeOy@users.noreply.github.com>
2018-05-22 16:34:44 -0400

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -5,16 +5,34 @@ import java.rmi.registry.Registry;
 
 
 public class Server extends Thread{
+	AddClass obj = null;
+	DatagramSocket socket = null;
 	private String city;
 	private int port;
+	private registry;
+	private int sock
 	
-	public Server(String ville, int porte){
+	public Server(String ville, int porte, int s){
+		obj = new AddClass();
 		city = ville;
 		port = porte;
+		sock = s;
 	}
-	
 	public static void run(){
-		
+		try{
+			registry = LocateRegistry.createRegistry(port);
+			registry.bind(city, obj);
+			socket = new DatagramSocket(sock);
+			byte[] buffer = new byte[1000];
+			while (true) {
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				socket.receive(request);
+				String count_s = Integer.toString(obj.getCount());
+				byte[] count_b = count_s.getBytes();
+				DatagramPacket reply = new DatagramPacket(count_b, count_b.length, request.getAddress(), request.getPort());
+				socket.send(reply);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception{
