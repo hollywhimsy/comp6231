3afdec26c6ccaf26410069b5497edd762452a585
qingouyang <qingouyang@qings-mbp.wireless.concordia.ca>
2018-05-22 21:14:43 -0400

    StudentRecord updated

--- src/common/TeacherRecord.java
+++ src/common/TeacherRecord.java
@@ -6,8 +6,14 @@ import people.Teacher;
  * Represents a teacher record
  */
 public class TeacherRecord extends Record {
+	private String specialization;
+	
 	public TeacherRecord(String first, String last) {
 		super(first, last);
+	}
+	
+	public String getSpecialization() {
+		return this.specialization;
 	}
 	
 }
