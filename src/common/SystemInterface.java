01abf88a986743539f1e1b1e8dd235af05553446
PancakeOy <39447361+PancakeOy@users.noreply.github.com>
2018-05-25 20:09:28 -0400

    Update SystemInterface.java

--- src/common/SystemInterface.java
+++ src/common/SystemInterface.java
@@ -8,7 +8,7 @@ public interface SystemInterface extends Remote {
 
     public void createTRecord(String firstName, String lastName, String address, String phone, String specialization, String location) throws RemoteException;
 
-    public void createSRecord(String firstName, String lastName, String courseRegistered, String status, String statusDate) throws RemoteException;
+    public void createSRecord(String firstName, String lastName, List<String> courseRegistered, String status, String statusDate) throws RemoteException;
 
     public Integer getRecordsCount() throws RemoteException;
 
