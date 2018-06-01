a1e94d545b2bebec26120d3fcde5cb62f4f922a3
William Prioriello <williamap@gmail.com>
2018-06-01 18:37:46 -0400

    added enum

--- src/runners/ServerRunnerAll.java
+++ src/runners/ServerRunnerAll.java
@@ -1,17 +1,18 @@
 package runners;
 
+import common.Constants;
 import network.CenterServer;
 
 public class ServerRunnerAll {
 
     public static void main(String[] args) {
-        CenterServer srvMTL = new CenterServer("MTL");
+        CenterServer srvMTL = new CenterServer(Constants.Locations.MTL.name());
         srvMTL.start();
 
-        CenterServer srvLVL = new CenterServer("LVL");
+        CenterServer srvLVL = new CenterServer(Constants.Locations.LVL.name());
         srvLVL.start();
 
-        CenterServer srvDDO = new CenterServer("DDO");
+        CenterServer srvDDO = new CenterServer(Constants.Locations.DDO.name());
         srvDDO.start();
     }
 
