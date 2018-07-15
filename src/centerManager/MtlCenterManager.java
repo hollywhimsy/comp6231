eaeec2f7f3f3a7c038508c35f6151592e6dd6daa
Sazadiabad <sazadi@gmail.com>
2018-07-14 20:09:59 -0400

    Project-Stage 1

---
+++ src/centerManager/MtlCenterManager.java
@@ -0,0 +1,32 @@
+package centerManager;
+
+import java.util.HashMap;
+import java.util.List;
+import common.Infrastucture;
+import common.Logger;
+import record.Record;
+import udp.UDPServer;
+
+public class MtlCenterManager extends Thread
+{
+	private String city;
+	private Logger logger;
+	private HashMap<Character, List<Record>> recordsMap = new HashMap<>();
+	
+	public MtlCenterManager(String city)
+	{
+		super();
+		this.city = city;
+		
+		logger = new Logger("SRV_" + city.toUpperCase().trim() + ".log");
+		logger.logToFile(city + "[MtlCenterManager Constructor]: " + city + " Center server is created :)");
+	}
+	
+	public void run()
+	{
+		// Start UDP CenterServer as a separate thread
+		Integer udpPort = Infrastucture.getServerPortUDP(city);
+		UDPServer srv = new UDPServer(recordsMap, udpPort, city, logger);
+		srv.start();
+	}
+}
--- src/rudp/Tester.java
+++ src/rudp/Tester.java
@@ -3,7 +3,6 @@ package rudp;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import common.Logger;
 import record.Record;
 
