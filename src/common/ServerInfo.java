ea943b9d0b0cdb8705cae8cb5784adbae00619fc
Valeriu Caramanuta <kavaleriu@gmail.com>
2018-07-25 19:32:31 -0400

    more details to server info

--- src/common/ServerInfo.java
+++ src/common/ServerInfo.java
@@ -6,6 +6,12 @@ public class ServerInfo {
 	private Integer udpPort;
 	private String host;
 
+	public ServerInfo(String location, String host, Integer udpPort) {
+		this.location = location;
+		this.host = host;
+		this.udpPort = udpPort;
+	}
+
 	public String getLocation() {
 		return location;
 	}
@@ -28,6 +34,27 @@ public class ServerInfo {
 
 	public void setHost(String host) {
 		this.host = host;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ServerInfo other = (ServerInfo) obj;
+		if ((other.getHost() != this.getHost()) || (other.getLocation() != this.getLocation())
+				|| (other.getUdpPort() != this.getUdpPort()))
+			return false;
+
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "  Location: " + location + "  host: " + host + "  port: " + udpPort.toString();
 	}
 
 }
